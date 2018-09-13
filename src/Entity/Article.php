ee6c2e65323194b8ec17101fb710fd59f4f49512
nanditapahar4896 <nanditasg4896@gmail.com>
2018-09-13 23:22:50 +0530

    Initial commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Body;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->Body;
+    }
+
+    public function setBody(string $Body): self
+    {
+        $this->Body = $Body;
+
+        return $this;
+    }
+}
